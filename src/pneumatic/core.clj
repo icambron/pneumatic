ce33aea85b0f883b127e3b6e6d096ef1aa8e9b14
Isaac Cambron <isaac@isaaccambron.com>
2014-12-15 09:40:45 -0500

    adding some tests

--- src/pneumatic/core.clj
+++ src/pneumatic/core.clj
@@ -4,7 +4,7 @@
 
 (defn- new-channels
   ([xfrom] (new-channels xfrom #(async/chan)))
-  ([xfrom channel-creator] (repeat (count xfrom) (channel-creator))))
+  ([xfrom channel-creator] (map (fn [_] (channel-creator)) xfrom)))
 
 ;; utilities
 
@@ -18,9 +18,9 @@
 (defn restructure
   "Blunt instrument for reorganizing sequences."
   [from to]
-  (fn [xs]
+  (fn [& xs]
     (let [mapping (zipmap (flatten from) (flatten xs))]
-         (walk/prewalk-replace mapping to))))
+      (walk/prewalk-replace mapping to))))
 
 (defn full-spread [xs] (map vector xs))
 
@@ -36,8 +36,8 @@
   ([xf] (xduce xf 1))
   ([xf n] (fn [xfrom]
              (let [xto (new-channels xfrom)]
-                  (doseq [[from to] [xfrom xto]]
-                         (async/pipeline n from xf to))
+                  (doseq [[from to] (map list xfrom xto)]
+                         (async/pipeline n to xf from))
                   xto))))
 
 (defn slim
@@ -46,32 +46,33 @@
   (guarantee-size 1 #(vector (func (first %)))))
 
 (defn pure
-  "Convenience for using a pure function as a stage. You should really just using a transducer, which is all this does for you."
+  "Convenience for using a pure function as a stage. You should really just using a mapping transducer, which is all this does for you."
   ([func] (pure func 1))
-  ([func n] (xduce (async/map func) n)))
+  ([func n] (xduce (map func) n)))
 
-(defn bury
-  "Drop all channels"
-  [] (fn [xfrom] (vector)))
+(defn bury-all
+  "Drop all channels, useful when combined with shunt"
+  []
+  (fn [xfrom] []))
 
 (defn terminate [func]
   "Convenience that creates a stage out of a function that takes a vector of input channels and returns nothing relevant."
   (fn [xfrom]
     (func xfrom)
-    (vector)))
+    []))
 
 (defn divvy-by
-  "Split up the input stages into different buckets and supply each bucket to different stages. The bucketization is controlled by `restructurer`, a function like [ch] -> [[ch]]. The vector returned by `restructurer` must be the same length as `stages`. If there are fewer stages than buckets, additional channels are flattened and passed on untouched."
-  [restructurer & stages]
+  "Split up the input stages into different buckets and supply each bucket to different stages. The bucketization is controlled by `which`, a function like [ch] -> [[ch]]. The vector returned by `which` must be at least the length as `stages`. If there are fewer stages than buckets, additional channels are flattened and passed on untouched."
+  [which & stages]
   (fn [xfrom]
-    (let [restructured      (restructurer xfrom)
+    (let [restructured      (which xfrom)
           re-count          (count restructured)
           stage-count       (count stages)
           [staged leftover] (split-at stage-count xfrom)]
 
          (assert (<= re-count stage-count) "You must split the channels into at at least the number of buckets as you provide stages to handle them.")
          (->>
-           (map #(if (sequential? %) (flatten %) (vector %)) restructured)
+           (map #(if (sequential? %) (flatten %) [%]) restructured)
            (zipmap stages)
            (mapcat (fn [[stage chs]] (stage chs)))
            (#(concat % (flatten leftover)))
@@ -86,16 +87,17 @@
   ([stage] (shunt stage 1))
   ([stage n] (divvy-by (partial split-at n) stage)))
 
-(def inject
-  "Add another channel"
-  conj)
+(defn inject
+  "Add channels. If one argument is provided, the channels are added at the end of the vector. The second argument, `which`, is a function that takes to vectors of channels and returns a vector combining them."
+  ([chs] (fn [xfroms] (vec (concat xfroms chs))))
+  ([chs which] (fn [xfroms] (which xfroms chs))))
 
 ;; combine or split channels
 
 (defn mix-channels
-  "Mix subsets of the input channels together. The bucketization is controlled by `restructurer`, a function like [ch] -> [[ch]]"
-  ([restructurer] (mix-channels restructurer async/chan))
-  ([restructurer channel-creator & channel-args]
+  "Mix subsets of the input channels together. The bucketization is controlled by `which`, a function like [ch] -> [[ch]]"
+  ([which] (mix-channels which async/chan))
+  ([which channel-creator & channel-args]
     (fn [xfrom]
       (map (fn [from]
                (if (sequential? from)
@@ -119,7 +121,7 @@
   [xto]
   (fn [xfrom]
       (assert (= (count xto) (count xfrom)) "You must provide the same number of buffer channels as input channels")
-      (doseq [[from to] [xfrom xto]]
+      (doseq [[from to] (map list xfrom xto)]
         (async/pipe from to))
       xto))
 
@@ -131,12 +133,15 @@
          ((buffer-with xto) xfrom))))
 
 ;; todos
-(defn tap [func] (fn [xfrom] ()))
-(defn fork [n] (fn [xfrom] ()))
+(defn tap [func] (fn [xfrom] nil))
+(defn fork [n] (fn [xfrom] nil))
+(defn parallel [n xform] nil)
+(defn map-all [f] (fn [xform] nil))
+(defn bury-some [which] (fn [ xform] nil))
 
 ;; start!
 
 (defn start
-  "Takes a function `source-stage` that returns a vector of channels and any number of stages to pipe through"
-  [source-stage & stages]
-  ((apply chain stages) (source-stage)))
+  "Takes a vector of channels or function that returns a vector of channels and any number of stages to pipe through"
+  [src & stages]
+  ((apply chain stages) (if (fn? src) (src) src)))
--- test/pneumatic/core_test.clj
+++ test/pneumatic/core_test.clj
@@ -1,7 +1,106 @@
 (ns pneumatic.core-test
-  (:require [clojure.test :refer :all]
-            [pneumatic.core :refer :all]))
+  (:require [midje.sweet :refer :all]
+            [pneumatic.core :refer :all]
+            [clojure.core.async :as async :refer [go-loop go]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; poor example; use a transducer instead
+(defn- almost-adder [n]
+  (fn [in]
+    (let [out (async/chan)]
+      (async/go-loop []
+        (let [v (async/<! in)]
+          (if (nil? v)
+              (async/close! out)
+              (do
+                (async/>! out (+ n v))
+                (recur)))))
+      out)))
+
+(defn- adder [n]
+  (guarantee-size 1
+    (fn [[in]]
+      [((almost-adder n) in)])))
+
+(defn- numbers [] [(async/to-chan (take 10 (iterate inc 0)))])
+(defn- more-numbers [] [(async/to-chan (take 10 (iterate inc 12)))])
+
+(defn- sequentialize [channel]
+  (let [value (async/<!! channel)]
+    (if (nil? value) nil (cons value (lazy-seq (sequentialize channel))))))
+
+(defn- sequentialize-nth [channels n]
+  (-> channels (nth n) sequentialize))
+
+(defn- sequentialize-first [channels]
+  (sequential-nth channels 0))
+
+(defn- sequentialize-all [channels]
+  (map sequentialize channels))
+
+(fact "start"
+  (fact "combines a chain of stages with a source function"
+    (sequentialize-first (start numbers (adder 1) (adder 2))) => (take 10 (iterate inc 3)))
+  (fact "combines a chaine of stages with a source vector"
+    (sequentialize-first (start (numbers) (adder 1) (adder 2))) => (take 10 (iterate inc 3))))
+
+(fact "chain"
+  (fact "combines two stages serially"
+    (sequentialize-first ((chain (adder 1) (adder 2)) (numbers))) => (take 10 (iterate inc 3))))
+
+(fact "slim"
+  (fact "wraps a ch -> ch into a stage"
+    (sequentialize-first (start numbers (slim (almost-adder 3)))) => (take 10 (iterate inc 3))))
+
+(fact "pure"
+  (fact "maps a pure function"
+    (sequentialize-first (start numbers (pure (partial + 3)))) => (take 10 (iterate inc 3)))
+  (fact "maps a pure function in paraellel"
+    (sequentialize-first (start numbers (pure (partial + 3) 5))) => (take 10 (iterate inc 3)))
+  (fact "works on multiple channels"
+    (sequentialize-all (start (concat (numbers) (more-numbers)) (pure (partial + 3)))) => [(take 10 (iterate inc 3)) (take 10 (iterate inc 15))]))
+
+(fact "xduce"
+  (fact "applies the transducer"
+    (sequentialize-first (start numbers (xduce (filter even?)))) => (take 5 (iterate #(+ 2 %) 0)))
+  (fact "applies the transducer in parallel"
+    (sequentialize-first (start numbers (xduce (filter even?) 5))) => (take 5 (iterate #(+ 2 %) 0)))
+  (fact "works on multiple channels"
+    (sequentialize-all (start (concat (numbers) (more-numbers)) (xduce (filter even?)))) => [(take 5 (iterate #(+ 2 %) 0)) (take 5 (iterate #(+ 2 %) 12))]))
+
+(fact "bury-all"
+  (fact "ignores the pipeline"
+    (start numbers (bury-all)) => []))
+
+(fact "terminate"
+  (fact "executes the function but doesn't provide any channels"
+    (let [output (transient [])
+          terminator (fn [[ch]]
+                       (loop []
+                         (let [i (async/<!! ch)]
+                           (cond (not (nil? i))
+                                 (do
+                                   (conj! output i)
+                                   (recur))))))]
+        (start numbers (terminate terminator))
+        (persistent! output))
+    =>
+    (take 10 (iterate inc 0))))
+
+(fact "inject"
+  (fact "injects channels at the end by default"
+    (let [pipeline (start
+                      (concat (numbers) (numbers))
+                      (xduce (map (partial * 2)))
+                      (inject (numbers)))]
+      (sequentialize-nth pipeline 2) => (take 10 (iterate inc 0))))
+
+  (fact "injects channels in locations specified by the second argument"
+    (let [pipeline (start
+                      (concat (numbers) (numbers))
+                      (xduce (map (partial + 2)))
+                      (inject (numbers) (restructure [[:a :b] [:c]] [:a :c :b])))]
+      (count pipeline) => 3
+      (sequentialize-nth pipeline 0) => (take 10 (iterate inc 2))
+      (sequentialize-nth pipeline 1) => (take 10 (iterate inc 0))
+      (sequentialize-nth pipeline 2) => (take 10 (iterate inc 2)))))
+
